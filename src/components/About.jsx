2261210da2d4956c2e43468bc75a2cb8256cb83a
volx <97158976+vo1x@users.noreply.github.com>
2024-06-19 21:59:30 +0545

    Add about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,22 @@
-
 function About() {
   return (
     <div className="flex flex-col gap-4">
-        <div className="text-lg font-semibold text-slate-300">About</div>
-        <div className="text-slate-400">
-            This is my about section
-            This is my about section
-            This is my about section
-            This is my about section
-            This is my about section
-            This is my about section
-            This is my about section
-            This is my about section
-            This is my about section
-            This is my about section
-        </div>
+      <div className="text-lg font-semibold text-slate-300">About</div>
+      <div className="flex flex-col gap-2 text-slate-400">
+        <p>
+          Hello! I'm volx and I enjoy developing things. My interest in web development started back
+          in 2021 when I first tried making a website for my school project — surprisingly getting a
+          navbar to stick to the top is a great way to learn HTML and CSS.
+        </p>
+        <p>
+          Since then, I've expanded into JavaScript for interactivity and backend technologies like
+          Python and Django. I'm passionate about developing seamless user interfaces without
+          compromising the user experience.I enjoy experimenting with new ideas for personal
+          projects, and contributing to Open Source Projects.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
