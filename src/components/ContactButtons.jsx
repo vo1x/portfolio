8b344c9d8b326edc826ac10b4255b0b8f506b4a5
volx <97158976+vo1x@users.noreply.github.com>
2024-06-19 22:02:44 +0545

    Add hyperlinks

--- src/components/ContactButtons.jsx
+++ src/components/ContactButtons.jsx
@@ -3,13 +3,16 @@ import { DownloadIcon, MailIcon } from 'lucide-react';
 function ContactButtons() {
   return (
     <div className="flex w-max items-center gap-4">
-      <button className="flex w-52 items-center gap-4 rounded-md  border-t border-t-slate-700 bg-slate-800 p-2 text-slate-300 shadow-md">
+      <button
+        disabled
+        className="flex w-52 cursor-pointer items-center gap-4 rounded-md  border-t border-t-slate-700 bg-slate-800 p-2 text-slate-300 shadow-md"
+      >
         <span>
           <DownloadIcon></DownloadIcon>
         </span>
         <span>Download Resume</span>
       </button>
-      <button className="flex w-max items-center gap-4 rounded-md  border-t border-t-slate-700 bg-slate-800 p-2 text-slate-300 shadow-md">
+      <button className="flex w-max cursor-pointer items-center gap-4 rounded-md  border-t border-t-slate-700 bg-slate-800 p-2 text-slate-300 shadow-md">
         <MailIcon></MailIcon>
         <span>Email me!</span>
       </button>
--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -2,16 +2,28 @@ import { Github, LinkedinIcon, MailIcon } from 'lucide-react';
 import { LiaTelegramPlane } from 'react-icons/lia';
 function Socials() {
   return (
-    
-    <div className='flex items-center gap-6 text-slate-400'>
-      <div className='hover:text-white transition-all ease-in-out duration-200 cursor-pointer'>
-        <Github size={30} ></Github>
-      </div>
-      <div className='hover:text-white transition-all ease-in-out duration-200 cursor-pointer'>
-        <LiaTelegramPlane className='text-3xl'/>
-      </div>
-      <div className='hover:text-white transition-all ease-in-out duration-200 cursor-pointer'><LinkedinIcon size={30}></LinkedinIcon></div>
-      
+    <div className="flex items-center gap-6 text-slate-400">
+      <a
+        href="https://github.com/vo1x"
+        target="_blank"
+        className="cursor-pointer transition-all duration-200 ease-in-out hover:text-white"
+      >
+        <Github size={30}></Github>
+      </a>
+      <a
+        href="https://t.me/volx999"
+        target="_blank"
+        className="cursor-pointer transition-all duration-200 ease-in-out hover:text-white"
+      >
+        <LiaTelegramPlane className="text-3xl" />
+      </a>
+      <a
+        href=""
+        // target="_blank"
+        className="cursor-pointer transition-all duration-200 ease-in-out hover:text-white"
+      >
+        <LinkedinIcon size={30}></LinkedinIcon>
+      </a>
     </div>
   );
 }
